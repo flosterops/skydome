92266a407ad0832a0e2484eac5aa047dc4cc446e
Filip Kulpinski <filip.kulpinski@aeriagames.com>
2021-04-16 19:01:34 +0200

    Adds beta survey form validation and sending

--- assets/js/skydome.js
+++ assets/js/skydome.js
@@ -1,175 +1,261 @@
-!function(a,b,c,d){a.fn.doubleTapToGo=function(d){return!!("ontouchstart"in b||navigator.msMaxTouchPoints||navigator.userAgent.toLowerCase().match(/windows phone os 7/i))&&(this.each(function(){var b=!1;a(this).on("click",function(c){var d=a(this);d[0]!=b[0]&&(c.preventDefault(),b=d)}),a(c).on("click touchstart MSPointerDown",function(c){for(var d=!0,e=a(c.target).parents(),f=0;f<e.length;f++)e[f]==b[0]&&(d=!1);d&&(b=!1)})}),this)}}(jQuery,window,document);
+!(function (a, b, c, d) {
+  a.fn.doubleTapToGo = function (d) {
+    return (
+      !!(
+        "ontouchstart" in b ||
+        navigator.msMaxTouchPoints ||
+        navigator.userAgent.toLowerCase().match(/windows phone os 7/i)
+      ) &&
+      (this.each(function () {
+        var b = !1;
+        a(this).on("click", function (c) {
+          var d = a(this);
+          d[0] != b[0] && (c.preventDefault(), (b = d));
+        }),
+          a(c).on("click touchstart MSPointerDown", function (c) {
+            for (
+              var d = !0, e = a(c.target).parents(), f = 0;
+              f < e.length;
+              f++
+            )
+              e[f] == b[0] && (d = !1);
+            d && (b = !1);
+          });
+      }),
+      this)
+    );
+  };
+})(jQuery, window, document);
 
-(function(){
-
-    $(document).on('submit', 'form.registration-form' , function(e) {
-        e.preventDefault();
-        var data = JSON.stringify({
-            "channel":"SKYDOME", 
-            "email": $('#email-newsletter').val(), 
-            "language":"en-US"
-        });
-        var xhr = new XMLHttpRequest();
-        var url = "https://newsletter.trionworlds.com/api/subscribe";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    /*GmgSession.sendClickedRegisterEvent('', 'Landing Page');*/
-                    $('#modal').toggleClass('open');
-                    $('#modal-overlay').toggleClass('open');
-                    $('#modal-text').text("Thank you for your interest in Skydome Beta! Check your inbox for a confirmation email.");
-                    document.getElementById("newsletter-form").reset();
-                    document.getElementById("confirmation").checked = false;
-                } else {
-                    $('#modal').toggleClass('open');
-                    $('#modal-overlay').toggleClass('open');
-                    $('#modal-text').text("Error, please try again later.");
-                }  
-            }
-        }
-    xhr.send(data);
+(function () {
+  $(document).on("submit", "form.registration-form", function (e) {
+    e.preventDefault();
+    var data = JSON.stringify({
+      channel: "SKYDOME",
+      email: $("#email-newsletter").val(),
+      language: "en-US",
     });
-
-}());
-
-$(document).ready(function(){
-	
-	$('.dropdown').doubleTapToGo();
-	/*if($('.sidebar').length>0){
-		$(".sidebar").sidebar("attach events",".toc"),$(".sidebar a.item").on("click",function(){$(".sidebar").sidebar("hide")});
-	};*/
-
-	$("#close-button").click(function() {
-		$('#modal').toggleClass('open');
-		$('#modal-overlay').toggleClass('open');
-	});
-	
-	$(function() {
-		var Accordion = function(el, multiple) {
-			this.el = el || {};
-			this.multiple = multiple || false;
-	
-			// Variables privadas
-			var links = this.el.find('.link');
-			// Evento
-			links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-		}
-	
-		Accordion.prototype.dropdown = function(e) {
-			var $el = e.data.el;
-				$this = $(this),
-				$next = $this.next();
-	
-			$next.slideToggle();
-			$this.parent().toggleClass('open');
-	
-			if (!e.data.multiple) {
-				$el.find('.desc').not($next).slideUp().parent().removeClass('open');
-			};
-		}	
-	
-		var accordion = new Accordion($('#accordion'), false);
-	});
-	
-	window.lazySizesConfig = window.lazySizesConfig || {};
-	window.lazySizesConfig.lazyClass = 'lazyload';
-		
-	$('.n_about').on("click", function(){
-		$('html, body').animate({scrollTop: $('#game').offset().top-50}, 1000);
-	});
-
-	$('.n_features').on("click", function(){
-		$('html, body').animate({scrollTop: $('#features').offset().top-75}, 1000); 
-	});
-	
-	$('.n_media').on("click", function(){
-		$('html, body').animate({scrollTop: $('#media').offset().top-85}, 1000);
-	});	 
-	
-	$('.signup, .signup-now').on("click", function(){
-		$('html, body').animate({scrollTop: $('#signup').offset().top+320}, 1000);
-	})
-
-	$('#totop').click(function(event){
-		$('html,body').animate({scrollTop:0},1000);
-	});
-	
-	/*if($(document).width() > "768") {
-	$('.screenshots').slick({  
-		centerMode: true,
-		centerPadding: '500px',
-		slidesToShow: 1,
-		autoplay: true,
-		autoplaySpeed: 5000,
-		dots: true,
-		arrows: false,
-		mobileFirst: true,
-		responsive: [
-			{
-				breakpoint: 1600,
-				settings: {
-				centerMode: true,
-				centerPadding: '500px',
-				slidesToShow: 1
-				}
-			},
-			{
-				breakpoint: 1200,
-				settings: {
-				centerMode: true,
-				centerPadding: '300px',
-				slidesToShow: 1
-				}
-			},
-			{
-			breakpoint: 768,
-			settings: {
-				centerMode: true,
-				centerPadding: '100px',
-				slidesToShow: 1
-				}
-			 },
-		]
-	});
-	}*/
-
-	splide = new Splide( '#splide', {
-		type: 'loop',
-		arrows: false,
-		autoWidth: true,
-		pagination: true,
-		lazyLoad: 'nearby',
-		focus    : 'center',
-		breakpoints: {
-			480: {
-				destroy: true, 
-			},
-		}
-	  } ).mount();
-
-	var params = new URLSearchParams(window.location.search);
-	var token = params.get("token");
-	if(token) {
-		var data = JSON.stringify({
-            "token": token
-        });
-        var xhr = new XMLHttpRequest();
-        var url = "https://newsletter.trionworlds.com/api/subscribeDoubleOptIn";
-	
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-				if(xhr.status === 200) {
-                    /*GmgSession.sendRegistrationEvent('', 'Landing Page');*/
-					/*var urlConfirmation = new URL(window.location.origin)+ 'confirmation.html';
-					window.open(urlConfirmation);*/
-					window.location.href = "https://playskydome.com/confirmation.html";
-				} 
-            } 
+    var xhr = new XMLHttpRequest();
+    var url = "https://newsletter.trionworlds.com/api/subscribe";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          /*GmgSession.sendClickedRegisterEvent('', 'Landing Page');*/
+          $("#modal").toggleClass("open");
+          $("#modal-overlay").toggleClass("open");
+          $("#modal-text").text(
+            "Thank you for your interest in Skydome Beta! Check your inbox for a confirmation email."
+          );
+          document.getElementById("newsletter-form").reset();
+          document.getElementById("confirmation").checked = false;
+        } else {
+          $("#modal").toggleClass("open");
+          $("#modal-overlay").toggleClass("open");
+          $("#modal-text").text("Error, please try again later.");
         }
-    	xhr.send(data);		
-	}	
-});	
+      }
+    };
+    xhr.send(data);
+  });
+
+  $(document).on("submit", "form#beta-survey-form", function (ev) {
+    ev.preventDefault();
+
+    const version = $("input[name=version]").val();
+    const language = "en";
+    const storeToken =
+      "aXZ-YXoyX0lkR1RWVGRRVjYzOGYtNnhRUSZkYXRhfjFfdmZTX3hQMFIxT2hVTXFvUkFGNk1SMlJGckp6d3hCbnhidUlJc3hFNTNfSXFhbkEwVS1WRjc1MjA2TnhVM3ZMYU9aRE5WSXZzWWUwZUE4TkZqRFprUDFJVVdSbkJsYmlnUWtKa2Fmb1RfN2o5QXNFc2p4cXlHM1EwbVJrUXYxazhIOUpEODZndGotWW9laExUS053VGJLUzZzc2ZxdURTSmw3ZEdFNHRHOEdjSTAzcHRmZHNtYzJ2dGpZVVBfSXRFY1dlSWFLd1lZdHlTcGFzWkhmTDRWSXdhbFdXWW85VjVJUm44ZmNjbjRGcW83eEhySTVWblVIRFVGd1d2YU14emVCdjZKUExxY2hWaEpIa3FWeFhoQnFFMDF0QzJ4R0xKUEJ0ZE1CSTNWcFNycG9BTVNwcm1BUGN2ZU52TEt4V1kxRnAta2NfWktaWEVyc3Q5SkJlZkluZTcyek9xTGlvb3o3VHJvMEVFWWtYM2FkY3hSbzNBd2pqUVFJY25MMnBPWjFWdUhqQ0lyUDhla0kxY2JBd0FpTFFMMHB5cnQ3bU5FUFc0QjZoRlZTcVZLV0lKMG0zLXpsUHlNR01zVm5MdWJvWjlSZEp6WGt0V3FWVWw3TlZQZTAxd3psSzc0cjNIajJYekw3ajQyNm8yMkpRQkp4WkJnanZaci1ObnBNWG1SbHQ1M3pyMG5ablVvYVQyUDdoQQAA";
+    const phone = $("input[name=phone]").val();
+    const data = $(this).serializeArray();
+    const questionsCount = $(".survey-questions > li").length - 1;
+    var answers = {};
+
+    // transform form data into answers object
+    data
+      .filter((e) => !isNaN(e.name) && e.value !== "")
+      .forEach((e) => {
+        if (answers[e.name] === undefined) {
+          answers[e.name] = e.value;
+        } else {
+          answers[e.name] += ", " + e.value;
+        }
+      });
+
+    // verify answer count
+    if (Object.keys(answers).length !== questionsCount) {
+      alert("Please, answer all questions first.");
+      //return;
+    }
+
+    // display loader
+    $(".survey-wrapper").hide();
+    $(".survey-loader").removeClass("hidden");
+
+    // send request
+    $.ajax({
+      url: `https://betasurvey.draft.int.one.gamigo.com/api/survey/${version}/${language}/?storeToken=${storeToken}&phone=${phone}`,
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(answers),
+    })
+      .done(function () {
+        $(".survey-result-success").removeClass("hidden");
+      })
+      .fail(function () {
+        $(".survey-result-failure").removeClass("hidden");
+      })
+      .always(function () {
+        $(".survey-loader").hide();
+      });
+  });
+})();
+
+$(document).ready(function () {
+  $(".dropdown").doubleTapToGo();
+  /*if($('.sidebar').length>0){
+        $(".sidebar").sidebar("attach events",".toc"),$(".sidebar a.item").on("click",function(){$(".sidebar").sidebar("hide")});
+    };*/
+
+  $("#close-button").click(function () {
+    $("#modal").toggleClass("open");
+    $("#modal-overlay").toggleClass("open");
+  });
+
+  $(function () {
+    var Accordion = function (el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+
+      // Variables privadas
+      var links = this.el.find(".link");
+      // Evento
+      links.on(
+        "click",
+        { el: this.el, multiple: this.multiple },
+        this.dropdown
+      );
+    };
+
+    Accordion.prototype.dropdown = function (e) {
+      var $el = e.data.el;
+      ($this = $(this)), ($next = $this.next());
+
+      $next.slideToggle();
+      $this.parent().toggleClass("open");
+
+      if (!e.data.multiple) {
+        $el.find(".desc").not($next).slideUp().parent().removeClass("open");
+      }
+    };
+
+    var accordion = new Accordion($("#accordion"), false);
+  });
+
+  window.lazySizesConfig = window.lazySizesConfig || {};
+  window.lazySizesConfig.lazyClass = "lazyload";
+
+  $(".n_about").on("click", function () {
+    $("html, body").animate({ scrollTop: $("#game").offset().top - 50 }, 1000);
+  });
+
+  $(".n_features").on("click", function () {
+    $("html, body").animate(
+      { scrollTop: $("#features").offset().top - 75 },
+      1000
+    );
+  });
+
+  $(".n_media").on("click", function () {
+    $("html, body").animate({ scrollTop: $("#media").offset().top - 85 }, 1000);
+  });
+
+  $(".signup, .signup-now").on("click", function () {
+    $("html, body").animate(
+      { scrollTop: $("#signup").offset().top + 320 },
+      1000
+    );
+  });
+
+  $("#totop").click(function (event) {
+    $("html,body").animate({ scrollTop: 0 }, 1000);
+  });
+
+  /*if($(document).width() > "768") {
+    $('.screenshots').slick({  
+        centerMode: true,
+        centerPadding: '500px',
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                centerMode: true,
+                centerPadding: '500px',
+                slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                centerMode: true,
+                centerPadding: '300px',
+                slidesToShow: 1
+                }
+            },
+            {
+            breakpoint: 768,
+            settings: {
+                centerMode: true,
+                centerPadding: '100px',
+                slidesToShow: 1
+                }
+             },
+        ]
+    });
+    }*/
+
+  splide = new Splide("#splide", {
+    type: "loop",
+    arrows: false,
+    autoWidth: true,
+    pagination: true,
+    lazyLoad: "nearby",
+    focus: "center",
+    breakpoints: {
+      480: {
+        destroy: true,
+      },
+    },
+  }).mount();
+
+  var params = new URLSearchParams(window.location.search);
+  var token = params.get("token");
+  if (token) {
+    var data = JSON.stringify({
+      token: token,
+    });
+    var xhr = new XMLHttpRequest();
+    var url = "https://newsletter.trionworlds.com/api/subscribeDoubleOptIn";
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          /*GmgSession.sendRegistrationEvent('', 'Landing Page');*/
+          /*var urlConfirmation = new URL(window.location.origin)+ 'confirmation.html';
+                    window.open(urlConfirmation);*/
+          window.location.href = "https://playskydome.com/confirmation.html";
+        }
+      }
+    };
+    xhr.send(data);
+  }
+});
